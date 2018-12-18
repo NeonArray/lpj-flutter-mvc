c40737fe990a853729bcaf2365907d344e07af49
Aaron Arney <aaron.g.arney@gmail.com>
2018-12-18 08:45:24 -0500

    Add helper methods to media query

--- lib/utility/media_query.dart
+++ lib/utility/media_query.dart
@@ -5,3 +5,14 @@ double getDeviceTargetWidth(BuildContext context) {
 	final double deviceWidth = MediaQuery.of(context).size.width;
 	return deviceWidth > 768.0 ? 500.0 : deviceWidth * 0.85;
 }
+
+Map<String, double> getViewportDimensions(BuildContext context) {
+	return {
+		'height': MediaQuery.of(context).size.height,
+		'width': MediaQuery.of(context).size.width,
+	};
+}
+
+double getDeviceHeight(BuildContext context) {
+	return MediaQuery.of(context).size.height;
+}
